067bd121041cb7862208390031dd078f36deb39f
Patrick Traynor <patrickjtraynor@gmail.com>
2019-01-25 21:17:16 -0800

    Basic Unity project with arrow key movement

---
+++ hermitcrab/Assets/Scripts/Crab.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Crab : MonoBehaviour {
+    void Update() {
+        Vector3 inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        transform.position += inputVector * 0.1f;
+    }
+}
