7fd0da18a7ba0002d74a9aaa1a4cee72a961ca33
Patrick Traynor <patrickjtraynor@gmail.com>
2019-01-26 19:44:40 -0800

    Title scene

---
+++ hermitcrab/Assets/Scripts/Title.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+    void Update() {
+        if (Input.GetButtonDown("Action")) {
+            SceneManager.LoadScene("TestScene");
+        }
+    }
+}
